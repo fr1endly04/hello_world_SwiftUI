1ae2b6db40f8d131b4fbf594a02ba8758356da3b
Johnny <nikusor.94@gmail.com>
2022-01-08 02:11:55 +0200

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Ciobanasu Ion on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
